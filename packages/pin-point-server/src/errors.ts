98a10becb026849c3764a3a423fada323c03ccce
tonytangdev <tony.tang.developer@gmail.com>
2026-04-10 16:31:37 +0200

    feat(server): add auth error types

--- packages/pin-point-server/src/errors.ts
+++ packages/pin-point-server/src/errors.ts
@@ -11,3 +11,15 @@ export class DatabaseError extends Data.TaggedError("DatabaseError")<{
 export class ValidationError extends Data.TaggedError("ValidationError")<{
 	readonly message: string;
 }> {}
+
+export class TokenNotFound extends Data.TaggedError("TokenNotFound")<{
+	readonly id: string;
+}> {}
+
+export class Unauthorized extends Data.TaggedError("Unauthorized")<{
+	readonly reason: string;
+}> {}
+
+export class Forbidden extends Data.TaggedError("Forbidden")<{
+	readonly reason: string;
+}> {}
